80da5a790ebba23e0ed2b4aeed4615119b3d1137
yue-github <1079051908@qq.com>
2020-07-25 08:14:21 +0800

    init

---
+++ src/config/system.js
@@ -0,0 +1,18 @@
+/**
+ * Created by OXOYO on 2019/5/18.
+ *
+ * 系统相关配置
+ */
+const { version, name, author, description } = require('../../package')
+export default {
+  version,
+  name,
+  author,
+  description,
+  title: 'WTY',
+  logo: require('@/assets/images/clipart/Earth_globe_128x128.png'),
+  github: 'https://github.com/yue-github',
+  site: 'https://coral3.com',
+  feedback: 'https://coral3.com',
+  copyright: ''
+}
